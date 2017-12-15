624305464b584cec00461e97512e7a5e963edc02
Александра <31474158+bennett-ray@users.noreply.github.com>
2017-12-15 11:58:16 +0300

    Add files via upload
    Added features from wikipedia to validation.txt, tested data on 5 pre-trained models

--- main.py
+++ main.py
@@ -9,40 +9,45 @@ import gensim
 from gensim.models import word2vec
 import time
 from gensim.models.keyedvectors import KeyedVectors
-# word_vectors = KeyedVectors.load_word2vec_format('/tmp/vectors.txt', binary=False)  # C text format
-
 
 def testCorpus(path_, split = 100):
     print('loading corpus')
-    a = []
-    spl = 0
-    f = open(path_, 'r', encoding='utf8')
-    for line in f:
+    result, spl = [], 0
+    file = open(path_, 'r', encoding='utf8')
+    for line in file:
         if spl != split:
-            l = line.split(',')
-            a.append([l[0],l[1],l[2],int(l[3])])
+            element = line.split(',')
+            result.append([element[0], element[1], element[2], int(element[3]), int(element[4]), int(element[5])])
         else:
             break
     print('test corpus loaded!')
-    return a
-
+    return(result)
 
 if __name__ == '__main__':
-    path_ = 'E:\hackaton\word2vec_models\GoogleNews-vectors-negative300.bin'
-    word_vectors = KeyedVectors.load_word2vec_format(path_, binary=True)
-    # word_vectors.init_sims(replace=True)
-    # print(word_vectors.doesnt_match("breakfast cereal dinner lunch".split()))
-    a = testCorpus('E:/hackaton/hackathon/train.txt')
-    # print(a[0])
-    for i in a:
+    test_corpus = testCorpus('validation_wiki.txt')
+    
+    path_ = 'glove.6B.100d.txt'
+    word_vectors = KeyedVectors.load_word2vec_format(path_, binary=False)
+
+    X, y = [], []
+    for group in test_corpus:
         try:
-            sim_0 = word_vectors.similarity(i[0], i[1])
-            sim_1 = word_vectors.similarity(i[1], i[2])
-            sim_2 = word_vectors.similarity(i[0], i[2])
-            print(sim_0, sim_1, sim_2)
-            print(i)
-            print(word_vectors.doesnt_match(i[:3]), i[3])
-            print('')
+            sim_01 = word_vectors.similarity(group[0], group[1])
+            sim_12 = word_vectors.similarity(group[1], group[2])
+            sim_02 = word_vectors.similarity(group[0], group[2])
+            wiki_3_in_1 = group[4]
+            wiki_3_in_2 = group[5]
+            if word_vectors.doesnt_match(group[:3]) == group[2]:
+                third_wheel = 1
+            else:
+                third_wheel = 0
+            
+            X.append((sim_01, sim_12, sim_02, wiki_3_in_1, wiki_3_in_2, third_wheel))
+            y.append(group[3])
+            
         except Exception as e:
-            print('things went bad due to this little rat: %s'%e)
+            pass
+            print('things went bad due to this little rat: %s'%e)        
+        
+
 
